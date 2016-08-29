4d05729fdac8a946248705f6bcfc1ca66266677a
Nathan Flew <nath@nathf.com>
2016-08-29 15:03:01 +1000

    Update app package and initial view

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,58 +1,80 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const devBuild = process.env.NODE_ENV !== 'production';
-const nodeEnv = devBuild ? 'development' : 'production';
+const devBuild = process.env.NODE_ENV !== 'production'
+const nodeEnv = devBuild ? 'development' : 'production'
 
-config = {
+const config = {
   entry: [
     'es5-shim/es5-shim',
     'es5-shim/es5-sham',
     'babel-polyfill',
-    './app/bundles/HelloWorld/startup/HelloWorldApp',
+    './app/bundles/transition/startup/HelloWorldApp'
   ],
 
   output: {
     filename: 'webpack-bundle.js',
-    path: '../app/assets/webpack',
+    path: '../app/assets/webpack'
   },
 
   resolve: {
+    root: [
+      path.resolve('./app')
+    ],
     extensions: ['', '.js', '.jsx'],
     alias: {
       react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
+      'react-dom': path.resolve('./node_modules/react-dom')
     },
+    modulesDirectories: ['node_modules']
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(nodeEnv),
-      },
-    }),
-  ],
   module: {
     loaders: [
       {
         test: require.resolve('react'),
-        loader: 'imports?shim=es5-shim/es5-shim&sham=es5-shim/es5-sham',
+        loader: 'imports?shim=es5-shim/es5-shim&sham=es5-shim/es5-sham'
       },
       {
         test: /\.jsx?$/, loader: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
-    ],
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$/i,
+        loader: 'file-loader?name=/img/[name].[ext]'
+      }
+    ]
   },
-};
+  sassLoader: {
+    outputStyle: devBuild ? 'nested' : 'compressed'
+  },
+  devtool: 'source-map',
+  plugins: [
+    new ExtractTextPlugin('[name].css?[hash]-[chunkhash]-[contenthash]', {
+      disable: false,
+      allChunks: true
+    }),
+    // new webpack.optimize.CommonsChunkPlugin('c', 'c.js'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv)
+      }
+    })
+  ]
+}
 
-module.exports = config;
+module.exports = config
 
 if (devBuild) {
-  console.log('Webpack dev build for Rails'); // eslint-disable-line no-console
-  module.exports.devtool = 'eval-source-map';
+  console.log('Webpack dev build for Rails') // eslint-disable-line no-console
+  module.exports.devtool = 'eval-source-map'
 } else {
   config.plugins.push(
     new webpack.optimize.DedupePlugin()
-  );
-  console.log('Webpack production build for Rails'); // eslint-disable-line no-console
+  )
+  console.log('Webpack production build for Rails') // eslint-disable-line no-console
 }
