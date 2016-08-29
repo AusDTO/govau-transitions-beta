4491fdac921c7513f95097aa0ac1cb5ee8e063c2
Nathan Flew <nath@nathf.com>
2016-08-29 15:09:39 +1000

    Adding stylesheet for compilation

--- client/app/bundles/transition/startup/HelloWorldApp.jsx
+++ client/app/bundles/transition/startup/HelloWorldApp.jsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import createStore from '../store/helloWorldStore';
 import HelloWorld from '../containers/HelloWorld';
 
+import 'assets/styles/main.scss'
+
 // See documentation for https://github.com/reactjs/react-redux.
 // This is how you get props from the Rails view into the redux store.
 // This code here binds your smart component to the redux store.
