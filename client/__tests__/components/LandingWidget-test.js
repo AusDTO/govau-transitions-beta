60a3d6e9fadcdb5f9d8adc326a38b4733ece117b
Nathan Flew <nath@nathf.com>
2016-08-30 14:12:27 +1000

    Updating to test to reflect latest

--- client/__tests__/components/LandingWidget-test.js
+++ client/__tests__/components/LandingWidget-test.js
@@ -1,16 +1,16 @@
 import test from 'ava';
 import React from 'react'
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 
 import LandingWidget from '../../app/components/LandingWidget'
 
 test('landing title will be displayed', t => {
-  const wrapper = shallow(<LandingWidget />)
-  t.is(wrapper.contains(<h2>Welcome to Transitions Beta!</h2>), true)
+  const wrapper = render(<LandingWidget name="some kind of" />)
+  t.is(wrapper.find('h2').text(), 'Welcome to some kind of Beta!')
 })
 
 test('the component will be wrapped by a `.container` class', t => {
   const wrapper = shallow(<LandingWidget />)
-  const fooInner = wrapper.find('.container')
-  t.is(fooInner.is('.container'), true)
+  const widgetInner = wrapper.find('.container')
+  t.is(widgetInner.is('.container'), true)
 })
