0d83a6c5563a2b47b706be5ede978a4a053ca30b
Nathan Flew <nath@nathf.com>
2016-08-31 10:29:14 +1000

    Add another base test

--- client/__tests__/reducers/landing-test.js
+++ client/__tests__/reducers/landing-test.js
@@ -4,3 +4,7 @@ import reducer from '../../app/reducers/landing'
 test('should return the inital state', t => {
   t.deepEqual(reducer(void 0, {}), { name: '' })
 })
+
+test('passing no action but updated state', t => {
+  t.deepEqual(reducer({ name: 'Bo' }, {}), { name : 'Bo' })
+})
