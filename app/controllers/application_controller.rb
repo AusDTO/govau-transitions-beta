a16461ef30734ca3e5215800980f2fcd681fda58
Nathan Flew <nathf@users.noreply.github.com>
2016-09-13 12:26:28 +1000

    Add CSRF token as cookie, future use with xhr requests (#19)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  after_filter :set_csrf_cookie
+
+  def set_csrf_cookie
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
+
   def answer_session_for(wizard)
     @answer_sessions ||= {}
     @answer_sessions[wizard] ||= find_or_create_answer_session(wizard)
+  end
+
+  protected
+
+  def verified_request?
+    super || valid_authenticity_token?(session, request.cookies['XSRF-TOKEN'])
   end
 
   private
