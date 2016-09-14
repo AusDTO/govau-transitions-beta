e81456dececfa382a8b9d6f14b646bfbe8d8ce17
Josh Mostafa <micapam@gmail.com>
2016-09-14 11:56:01 +1000

    Options are now interpolatable (TRS-32) (#22)

--- app/decorators/question_decorator.rb
+++ app/decorators/question_decorator.rb
@@ -2,6 +2,12 @@ class QuestionDecorator < Draper::Decorator
   include Interpolatable
   delegate_all
 
+  def options
+    object.options.collect do |option|
+      decorate_interpolatably option
+    end
+  end
+
   def prompt
     interpolate object.prompt
   end
