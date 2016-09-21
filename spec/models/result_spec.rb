dfa304736352be32d7fe6c6a1e97254173035c9a
Josh Mostafa <micapam@gmail.com>
2016-09-21 10:08:26 +1000

    Fixed a couple of specs

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Result, type: :model do
   it { is_expected.to belong_to :wizard }
   it { is_expected.to validate_presence_of :wizard }
-  it { is_expected.to validate_presence_of :title }
 
   describe '#result_category' do
     let(:result) { Fabricate :result, container: container }
